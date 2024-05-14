36f43c11e36424ca992e4cafb95f5214ce950011
Gustavo Melo <gustavopmelo@live.com>
2024-05-13 22:19:00 -0300

    removido variavel id não utilizada na função

--- t1-clean-arch/src/main/java/com/g5/t1cleanarch/aplicacao/dtos/AtualizaCustoMensalRequisicaoDTO.java
+++ t1-clean-arch/src/main/java/com/g5/t1cleanarch/aplicacao/dtos/AtualizaCustoMensalRequisicaoDTO.java
@@ -2,12 +2,10 @@ package com.g5.t1cleanarch.aplicacao.dtos;
 
 public class AtualizaCustoMensalRequisicaoDTO {
     private double custo;
-    private double id;
 
     public AtualizaCustoMensalRequisicaoDTO(double custo, int id) 
     {
         this.custo = custo;
-        this.id = id;
     }
 
     public double getCustoMensal() {
