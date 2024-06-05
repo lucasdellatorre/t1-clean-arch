c4c5f8625c10afa7c989a82f8c50d2e3684db802
lucasdellatorre <lucasdellatorre222@gmail.com>
2024-06-05 11:42:25 -0300

    change dataPagamento to dataValidade in RegistraPagamentoDTO

--- t1-clean-arch/src/main/java/com/g5/t1cleanarch/aplicacao/dtos/RegistraPagamentoDTO.java
+++ t1-clean-arch/src/main/java/com/g5/t1cleanarch/aplicacao/dtos/RegistraPagamentoDTO.java
@@ -6,12 +6,12 @@ import com.g5.t1cleanarch.dominio.enums.StatusPagamento;
 
 public class RegistraPagamentoDTO {
     private StatusPagamento status;
-    private LocalDate dataPagamento;
+    private LocalDate dataValidade;
     private double valorEstornado;
 
-    public RegistraPagamentoDTO(StatusPagamento status, LocalDate dataPagamento, double valorEstornado) {
+    public RegistraPagamentoDTO(StatusPagamento status, LocalDate dataValidade, double valorEstornado) {
         this.status = status;
-        this.dataPagamento = dataPagamento;
+        this.dataValidade = dataValidade;
         this.valorEstornado = valorEstornado;
     }
 
@@ -19,8 +19,8 @@ public class RegistraPagamentoDTO {
         return status;
     }
 
-    public LocalDate getDataPagamento() {
-        return dataPagamento;
+    public LocalDate getDataValidade() {
+        return dataValidade;
     }
 
     public double getValorEstornado() {
