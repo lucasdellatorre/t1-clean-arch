c4aac83287f5594d89abb4c3e3d955eb615a527b
gfgiaretta <gabriel.giaretta001@edu.pucrs.br>
2024-06-29 15:24:34 -0300

    gateway funcionando

---
+++ api-gateway/src/test/java/com/g5/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
